f87d93fb69b90705913ddeeb2f0bf4e80a3e9101
amarachimbakwe <bmamarachi@vt.edu>
2023-05-22 03:57:00 -0400

    added changes

--- gaze_ml_2023/src/index.js
+++ gaze_ml_2023/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { HashRouter  as Router, Route, Switch} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
+     <Router>
+    <Switch>
+      <Route path="/" component={App} />
+    </Switch>
+  </Router>,
 );
 
 // If you want to start measuring performance in your app, pass a function
