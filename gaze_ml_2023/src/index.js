4fc41281dcdb33da5e7123d70760da828d300f6c
amarachimbakwe <bmamarachi@vt.edu>
2023-05-22 02:13:56 -0400

    setting deployment

--- gaze_ml_2023/src/index.js
+++ gaze_ml_2023/src/index.js
@@ -5,23 +5,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <HashRouter>
-//       <App />
-//     </HashRouter>
-//   </React.StrictMode>
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
